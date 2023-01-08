822fe1ffe4d071b5c107295e54c904b816dfe691
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-07 21:19:39 -0700

    fixed spelling mistake

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name?  ");
         string first = Console.ReadLine();
         
-        Console.Write("What is your lase name?  ");
+        Console.Write("What is your last name?  ");
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
