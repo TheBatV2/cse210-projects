5f33ff1de7250a6ad79c3c331f17bfa7b9b10db3
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-07 21:17:27 -0700

    Changed it to read name like Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?  ");
+        string first = Console.ReadLine();
+        
+        Console.Write("What is your lase name?  ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+
+
     }
 }
