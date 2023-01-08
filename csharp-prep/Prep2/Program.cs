76e6ec4aed6e896af8c05318182a4fa5d1edafe2
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-07 21:34:39 -0700

    Completed the base grading program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+       Console.Write("What is you Grade Score? ");
+       string userInput = Console.ReadLine();
+       int number = int.Parse(userInput);
+
+       if (number >= 90)
+       {
+        Console.WriteLine("You have an A");
+       }  
+       else if (number >= 80)
+       {
+        Console.WriteLine("You Have a B");
+       }   
+        else if (number >= 70)
+       {
+        Console.WriteLine("You Have a C");
+       }   
+        else if (number >= 60)
+       {
+        Console.WriteLine("You Have a D");
+       }   
+       
+        else 
+       {
+        Console.WriteLine("You Have a F");
+       }   
+        }
 }
