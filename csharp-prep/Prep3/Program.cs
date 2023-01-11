641eafba9e6a31914ddcff4c22dba674b01e5e19
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-10 22:38:03 -0700

    Created the code, tried having it restart

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+        int userNumber = -1;
+        
+        
+
+        while(userNumber != number)
+        {
+            Console.WriteLine("Please guess a number between 1 and 100 to see if you can guess the magic number.");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == number)
+                {
+                    Console.WriteLine("Congrats you guessed the number!  Play again?  Y/N ");
+                    string userRestart = Console.ReadLine();
+                    
+                }
+
+            else if (userNumber > number)
+                {
+                    Console.Write("Lower, ");
+                }    
+
+            else
+                {
+                    Console.Write("Higher, ");
+                }    
+        } 
+
+
+
+        
+            
+        
     }
 }
