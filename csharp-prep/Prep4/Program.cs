6cfcb563b714bf8148c4e09d759928d1e6aa69d9
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-11 21:40:16 -0700

    finished pt4, looked at hint for addition symbols

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        do 
+        {
+        Console.WriteLine("Please enter a couple numbers, when finished enter 0. ");
+        userNumber = int.Parse(Console.ReadLine());
+        
+        numbers.Add(userNumber);
+
+          
+        } while (userNumber != 0);
+
+        int total = 0;
+    
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The Total is: {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The Largest number you entered was: {largest}");
+
+
+
     }
+
+
+
 }
