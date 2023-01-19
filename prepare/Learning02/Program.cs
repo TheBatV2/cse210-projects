b221e22b5c7ef9576a7a99506ffaf152b436becd
Spencer Barbre <spencerbarbre@gmail.com>
2023-01-18 20:47:23 -0700

    COMPLETED VARIABLES

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Wastewater Operator";
+        job1._companyName = "City of Blackfoot";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Concrete Finisher";
+        job2._companyName = "ConcreteCraft";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Spencer Barbre";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+    
     }
 }
