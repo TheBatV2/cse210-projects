f7c30046f45965f9bc72520a6ca1981b45063bd9
Spencer Barbre <spencerbarbre@gmail.com>
2023-02-25 18:09:15 -0700

    test

--- prove/Develop04/Reflect.cs
+++ prove/Develop04/Reflect.cs
@@ -5,6 +5,12 @@ public class Reflect : IntroOutro
 
     public Reflect(string intro, string discription, int time):base(intro, discription, time)
     {
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(time);
 
+        Thread.Sleep(3000);
+
+        DateTime currentTime = DateTime.Now;
+        if (currentTime < futureTime)
     }
 }
